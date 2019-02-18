a33c5c516239aa58c539df9d7243224b4d62f3c8
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 19:37:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'secrets/new'
+  get 'secrets/show'
 
   get 'sessions/new'
+  get 'sessions/create'
 
-  get 'secrets/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
