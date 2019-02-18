9d048eaeef3e739b17585162e9b7554df08b81d3
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 19:40:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    if !params[:name] || params[:name].empty?
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      
+    end
   end
+  
 end
